6d4fe9fa3a04c5383baecf9e0a208ded87f51a75
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-01-26 22:39:30 +0530

    variable file added with correction and more commands added. Use case for export command is added.

--- 03-variable.sh
+++ 03-variable.sh
@@ -4,3 +4,18 @@ student_name="Raju"
 
 echo student_name = $student_name
 echo student_name = ${student_name}
+
+DATE=2021-01-2026
+echo Good Morning, Today date is $DATE
+
+## command substitution
+
+DATE=$(date +%F)
+echo Good Morning, Today date is $DATE
+
+##Arithmetic Substitution
+EXPR1=$((2+3-4/7*8))
+echo EXPR1 OUTPUT = $EXPR1
+
+## TRYING TO ACCESS A VARIABLE FROM SHELL COMMAND LINE (E.G: wE ARE ASSIGNING SOME VALUES in server side at run time, So by using export command we can use that value.)
+echo course_name = $COURSE_NAME
