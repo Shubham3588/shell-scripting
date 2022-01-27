f5bc4450da35788804b9e16f091d5ba106b41c9a
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-01-27 09:49:04 +0530

    append the log file by >>

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -38,3 +38,6 @@
 
 echo "Installing Nginx"
 yum install nginx -y >/tmp/roboshop.log
+
+echo "Download Frontend Content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>/tmp/roboshop.log
