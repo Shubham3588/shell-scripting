6f1ef2471ff16fbabb5f0331a01b3a0f6f80ab8a
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-02-01 23:09:51 +0530

    create ec2.sh file added

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -2,7 +2,7 @@
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
-if [ -z "$(AMI_ID)" ]; then
+if [ -z "${AMI_ID}" ]; then
   echo "Unable to find image AMId"
 else
   echo AMI ID = ${AMI_ID}
