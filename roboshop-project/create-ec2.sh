ae7d746c8fc6debf03596b012d144e7230c4d210
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-02-01 23:02:40 +0530

    catalogue file added

---
+++ roboshop-project/create-ec2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+
+if [ -z "$(AMI_ID)" ]; then
+  echo "Unable to find image AMId"
+  exit
+fi
