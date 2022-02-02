99712a7274b698b39f93d2e3f36ac3a509ce1c10
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-02-02 23:16:06 +0530

    create ec2.sh file added

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -48,3 +48,22 @@ INSTANCE_CREATE() {
                                    "ResourceRecords": [{ "Value": "IPADDRESS"}]
   }}]
   }' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/"  >/tmp/record.json
+
+  ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" --output text | grep roboshop.internal  | awk '{print $1}' | awk -F / '{print $3}')
+  aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json --output text &>>$LOG
+  echo -e "\e[1m DNS Record Created\e[0m"
+}
+
+### Main Program
+
+
+if [ "$1" == "list" ]; then
+  aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  --output table
+  exit
+elif [ "$1" == "all" ]; then
+  for component in cart catalogue dispatch frontend mongodb mysql payment rabbitmq redis shipping user ; do
+    INSTANCE_CREATE ${component}
+  done
+else
+  INSTANCE_CREATE $1
+fi
