5ea0c73a3a8cdb963c59b5a4745bacaef8852a68
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-02-03 22:02:33 +0530

    function.sh file added

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -6,6 +6,7 @@ rm -f $LOG
 if [ "$1" == "list" ]; then
   aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  --output table
   exit
+fi
 
 INSTANCE_CREATE() {
   INSTANCE_NAME=$1
