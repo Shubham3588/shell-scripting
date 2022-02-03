aa73c1dd5433648638c0ad382a0c2dd7bdfd3f58
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-02-03 17:38:21 +0530

    function.sh file added

--- 07-functions.sh
+++ 07-functions.sh
@@ -6,13 +6,24 @@
 
 ## Declare a function
 SAMPLE(){
+  a=20
+  b=20
   echo "Welcome to DevOps Training"
   echo "Welcome to DevOps Training"
   echo "Welcome to DevOps Training"
   echo "Welcome to DevOps Training"
 
 }
-
+## Main Program
 ## Calling a function
+a=10
 SAMPLE
 SAMPLE
+b=10
+
+
+# 1. When you declare a variable in main program  , then function can access it and modify it
+# 2. When You declare a variable in function , then you can overwrite it in main program
+# 3. Functions will not access special variable inputs given to script , only main program can access them. Meaning special variables for inputs are different for function from main program.
+# 4. Cases we need to come back immediately from function to main program then in that case we use return command . return command is like exit command, where exit exits the script whereas return with exits the functions.
+
--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,29 +1,32 @@
 #The frontend is the service in RobotShop to serve the web content over Nginx.
 source components/common.sh #calling the common log file for reusing
+
 echo "Installing Nginx"
 yum install nginx -y &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32m Success\e[0m"
-  else
-  echo -e "\e[1;33m Failed\e[0m"
-exit
-fi
+STAT $?
+
 echo "Download Frontend Content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+STAT $?
 
 echo "Clean Old Content"
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+STAT $?
 
 echo "Extract Frontend Content"
 cd /tmp &>>$LOG_FILE
 unzip -o /tmp/frontend.zip &>>$LOG_FILE
+STAT $?
 
 echo "Copy extracted content to Nginx Path"
 cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
+STAT $?
 
 echo "Copy Nginx Roboshop Config"
 cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+STAT $?
 
 echo "Start Nginx Service"
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
+STAT $?
