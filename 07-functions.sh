ec2b7615ebac70101eecf384f053ff019d255454
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-02-03 13:18:48 +0530

    roboshop.sh file added

---
+++ 07-functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# a set of data to a name is called as variable
+
+# a set of commands to a name is called as function
+
+## Declare a function
+SAMPLE(){
+  echo "Welcome to DevOps Training"
+}
+
+## Calling a function
+SAMPLE
