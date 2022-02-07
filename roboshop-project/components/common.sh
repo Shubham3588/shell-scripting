9b57bbc6192b7b52d1c0d873645d55a8bc84cf3b
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-02-07 23:24:30 +0530

    mysql.sh file added

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -1,6 +1,9 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE  # It will clear the log every time before running.
 
+## BUG about reaching one endpoint , To fix this we are using this command
+rm -f /etc/yum.repos.d/endpoint.repo
+
 STAT() {
   if [ $? -eq 0 ]; then
     echo -e "\e[1;32m SUCCESS\e[0m"
